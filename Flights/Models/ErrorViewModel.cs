b3dfd67938c0b42ef7e53a90c15453bf40fc18b3
bessa_ <claudioafbessa@gmail.com>
2024-02-18 22:37:42 +0000

    add backend

---
+++ Flights/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FlightsSearchPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
