142b0aba63c579b6bba12ef8305ab19de1cab845
bessa_ <claudioafbessa@gmail.com>
2024-02-18 22:55:19 +0000

    frontend updated

---
+++ Flights/Controllers/FlightController.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightsSearchPortal.Controllers;
+
+public class HomeController : Controller
+{
+    
+}
--- Flights/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-
-namespace FlightsSearchPortal.Controllers;
-
-public class HomeController : Controller
-{
-    
-}
